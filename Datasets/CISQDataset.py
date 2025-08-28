9b6947cfa0fd80528fe725138d26a65895cd5722
mrpawolo <mrpawolo@gmail.com>
2025-08-28 10:19:22 +0200

    cisq

--- Datasets/CISQDataset.py
+++ Datasets/CISQDataset.py
@@ -34,7 +34,7 @@ class CISQDataset(Dataset):
     "contrast": "contrast"
   }
 
-  def __init__(self, path:Path, train:bool, transform:torch.nn.Module = None, testSize = 0.2, load_img = True) -> None:
+  def __init__(self, path:Path, train:bool, transform:torch.nn.Module = None, testSize = 0.2, seed = 21, load_img = True) -> None:
     super().__init__()
     self.train = train
     self.path = path
@@ -43,13 +43,22 @@ class CISQDataset(Dataset):
 
     self.data =  pd.read_csv(self.dataPath)
     length = len(self.data["dmos"])
-    
+
+
+    names =  [n for n in self.data["image"]]
+    unitque_names =np.unique(names)
+    n_train, n_test = train_test_split(unitque_names, test_size=testSize, random_state=seed, shuffle=True)
+    # print(n_train)
+    # print("names",names)
+    mask = np.isin(names, n_train) if train else np.isin(names, n_test) 
+
 
     self.indexes = np.arange(start=0, stop=length) 
+    self.indexes = self.indexes[mask]
 
-    i_train, i_test = train_test_split(self.indexes, test_size=testSize, random_state=21, shuffle=True)
+    # i_train, i_test = train_test_split(self.indexes, test_size=testSize, random_state=seed, shuffle=True)
 
-    self.indexes = i_train if train else i_test
+    # self.indexes = i_train if train else i_test
     
     self.transform = transform
     self.load_img = load_img
@@ -73,7 +82,9 @@ class CISQDataset(Dataset):
     else: 
       img = None
 
-    dmos = torch.tensor(dmos)
+    dmos = torch.tensor(dmos, dtype=torch.float32).unsqueeze(0)
 
     return (img, dmos)
      
+if __name__ == "__main__":
+  test_set = CISQDataset(CISQ_PATH, train=False,load_img=False)
